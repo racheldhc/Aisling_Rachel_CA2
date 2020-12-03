5e0a8e75f23c1bb6970b5ee28837ad27f4e7ca9b
Rachel Callaghan <rachelcallaghan81@gmail.com>
2020-12-03 21:36:28 +0000

    add images

--- app/src/main/java/com/example/aisling_rachel_ca2/detail/DetailViewModel.kt
+++ app/src/main/java/com/example/aisling_rachel_ca2/detail/DetailViewModel.kt
@@ -2,7 +2,17 @@ package com.example.aisling_rachel_ca2.detail
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.example.aisling_rachel_ca2.network.ShoppingItem
 
-class DetailViewModel(@Suppress("UNUSED_PARAMETER")marsProperty: ShoppingItem, app: Application) : AndroidViewModel(app) {
+class DetailViewModel(shoppingItem: ShoppingItem, app: Application) : AndroidViewModel(app) {
+    private val _selectedItem = MutableLiveData<ShoppingItem>()
+
+    val selectedItem: LiveData<ShoppingItem>
+        get() = _selectedItem
+
+    init {
+        _selectedItem.value = shoppingItem
+    }
 }
